600a0bf3965a801a7cf753fe2326305fb3295a87
Shaun Brown <shaun.brown@strapi.io>
2022-03-30 11:06:57 +0200

    Gatsby frontend testing

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/shaunbrown/Strapi/Strapi-test-projects/Gatsby-blog30Mar/my-project/frontend/.cache/dev-404-page.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/shaunbrown/Strapi/Strapi-test-projects/Gatsby-blog30Mar/my-project/frontend/src/pages/about.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/shaunbrown/Strapi/Strapi-test-projects/Gatsby-blog30Mar/my-project/frontend/src/pages/index.js")),
+  "component---src-templates-article-post-js": preferDefault(require("/Users/shaunbrown/Strapi/Strapi-test-projects/Gatsby-blog30Mar/my-project/frontend/src/templates/article-post.js"))
+}
+
